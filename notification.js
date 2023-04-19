04c4131f8a666b33e528beb62a0d35c0a8fecd62
GhanashyamShingate <g_shingate@fanshaweonline.ca>
2023-04-18 23:16:07 -0400

    Notification

---
+++ notification.js
@@ -0,0 +1,93 @@
+const notification = document.getElementById("btn");
+if ("Notification" in window && "serviceWorker" in navigator) {
+  notification.addEventListener("click", () => {
+    console.log("Permission:", Notification.permission);
+    //debugger;
+    switch (Notification.permission) {
+      case "denied":
+        notificationNotAllowed();
+        break;
+
+      case "default":
+        console.log("In default");
+        requestUserPermission();
+        //debugger;
+        break;
+
+      case "granted":
+        displayNotification();
+        //notificationButton.disabled = true;
+        //notificationButton.style.visibility = "hidden";
+        //newForm.style.display = "none";
+        //debugger;
+        break;
+    }
+  });
+} else {
+  notificationNotAllowed();
+}
+function notificationNotAllowed() {
+  console.log("Notifications not allowed!");
+  //notificationButton.disabled = true;
+  //notificationButton.style.visibility = "hidden";
+  //songaddform.style.display = "none";
+}
+function requestUserPermission() {
+  console.log("Requesting permission");
+  //debugger;
+  Notification.requestPermission().then((permission) => {
+    console.log("User choice", permission);
+    if (permission === "granted") {
+      displayNotification();
+    } else if (permission === "default") {
+      //requestUserPermission();
+      console.log("User Choice:", permission);
+    } else {
+      notificationNotAllowed();
+    }
+  });
+}
+function displayNotification() {
+  console.log("Showing Notification");
+  //   const Option = {
+  //     body: body,
+  //     icon: "/icons/android-chrome-36x36.png",
+  //     actions: [
+  //       { action: "Agree", title: "Agree" },
+  //       { action: "DisAgree", title: "Disagree" },
+  //     ],
+  //   };
+  //   //new Notification(title, Option);
+  //   navigator.serviceWorker.ready.then((registration) => {
+  //     registration.showNotification(title, Option);
+  //   });
+  //newForm.style.display = "none";
+  //
+  //debugger;
+}
+
+notification.addEventListener("click", () => {
+  if (Notification.permission === "granted") {
+    console.log("In showbutton");
+    const titleOfNotification = document.getElementById("Title").value;
+    const body = document.getElementById("body").value;
+    if (!titleOfNotification) {
+      alert("The title field is required.");
+    } else {
+      const Option = {
+        body: body,
+        icon: "/icons/android-chrome-36x36.png",
+        actions: [
+          { action: "Agree", title: "Agree" },
+          { action: "DisAgree", title: "Disagree" },
+        ],
+      };
+      //new Notification(title, Option);
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.showNotification(titleOfNotification, Option);
+      });
+    }
+  } else {
+    alert("Permission is not granted");
+  }
+});
